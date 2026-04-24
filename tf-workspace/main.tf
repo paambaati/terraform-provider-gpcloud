0b11fb6a44e1d292312ef54b2be546782b4d15d7
GP <me@httgp.com>
2026-04-24 10:10:57 +0530

    chore: cleanup files

--- tf-workspace/main.tf
+++ tf-workspace/main.tf
@@ -1,11 +1,14 @@
 terraform {
   required_providers {
-    gpcloud = { source = "paambaati/gpcloud" }
+    gpcloud = {
+      source = "paambaati/gpcloud"
+      version = "0.0.2"
+    }
   }
 }
 
 provider "gpcloud" {
-  # api_url = "http://127.0.0.1:8765"
+  api_url = "http://127.0.0.1:8765"
 }
 
 resource "gpcloud_item" "example" {
